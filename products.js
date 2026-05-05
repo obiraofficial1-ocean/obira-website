860d2707dcfd3e09de67653f4954c13efc7386bd
obiraofficial1-ocean <obiraofficial1@gmail.com>
2026-05-05 21:20:17 +0530

    Update products.js

---
+++ products.js
@@ -0,0 +1,17 @@
+const products = [
+
+{
+id:"p1",
+name:"OBIRA Aurelia Lock Bracelet",
+price:1199,
+image:"images/Bracelets-ALB-001.jpg"
+},
+
+{
+id:"p2",
+name:"OBIRA Chic Radiance Bracelet",
+price:1199,
+image:"images/Bracelets-CR-002.jpg"
+}
+
+];

31598d4f4df34353414070f71e8e11d471859355
obiraofficial1-ocean <obiraofficial1@gmail.com>
2026-05-05 20:57:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -133,3 +133,106 @@ renderStore();
 loadProductPage();
 updateCartCount();
 });
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+// CART
+function addToCart(id){
+let product = products.find(p=>p.id===id);
+cart.push(product);
+localStorage.setItem("cart", JSON.stringify(cart));
+updateCartCount();
+alert("Added to cart");
+}
+
+function updateCartCount(){
+document.querySelectorAll("#cart-count").forEach(el=>{
+el.innerText = cart.length;
+});
+}
+
+// STORE LOAD
+function loadStore(){
+let container = document.getElementById("store-products");
+if(!container) return;
+
+container.innerHTML = "";
+
+products.forEach(p=>{
+container.innerHTML += `
+<div class="product-card">
+<img src="${p.image}" onclick="openProduct('${p.id}')">
+<h3>${p.name}</h3>
+<p>₹${p.price}</p>
+<button onclick="addToCart('${p.id}')">Add to Cart</button>
+</div>
+`;
+});
+}
+
+// PRODUCT PAGE
+function openProduct(id){
+window.location.href = "product.html?id="+id;
+}
+
+function loadProduct(){
+let id = new URLSearchParams(window.location.search).get("id");
+let p = products.find(x=>x.id===id);
+if(!p) return;
+
+document.getElementById("p-name").innerText = p.name;
+document.getElementById("p-price").innerText = "₹"+p.price;
+document.getElementById("p-img").src = p.image;
+}
+
+// CART PAGE
+function loadCart(){
+let container = document.getElementById("cart-items");
+let total = 0;
+
+if(!container) return;
+
+container.innerHTML="";
+
+cart.forEach(item=>{
+total+=item.price;
+container.innerHTML+=`
+<div class="cart-item">
+<img src="${item.image}">
+<p>${item.name}</p>
+<p>₹${item.price}</p>
+</div>
+`;
+});
+
+document.getElementById("total").innerText = total;
+}
+
+// PAYMENT
+function checkout(){
+
+let total = cart.reduce((s,i)=>s+i.price,0);
+
+var options = {
+"key": "rzp_test_SljBi0Hjg0lqGY",
+"amount": total*100,
+"currency": "INR",
+"name": "OBIRA",
+"description": "Order Payment",
+"handler": function (){
+localStorage.removeItem("cart");
+window.location.href="success.html";
+}
+};
+
+var rzp = new Razorpay(options);
+rzp.open();
+}
+
+// INIT
+document.addEventListener("DOMContentLoaded", ()=>{
+updateCartCount();
+loadStore();
+loadProduct();
+loadCart();
+});
